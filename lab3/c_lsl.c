4edbbfdbf2748ac38201797392b730f2afa2ff03
Andy Lahs <andy.lahs@gmail.com>
2017-02-10 15:01:38 +0000

    work on part 7

--- lab3/c_lsl.c
+++ lab3/c_lsl.c
@@ -14,7 +14,8 @@ int file_select();
 
 extern int alphasort();
 
-main() {
+int main(void)
+{
     int count, i;
     struct direct **files;
 
@@ -22,19 +23,22 @@ main() {
     int t = 1;
     getcwd(pathname);
     printf("PWD = %s\n", pathname);
-    count = scandir(pathname, &files, file_select, alphasoxrt);
+    count = scandir(pathname, &files, file_select, alphasort);
     /* No files in Dir */
-    if (count <= 0) {
+    if (count <= 0)
+    {
         printf("No files in Dir\n");
         exit(0);
     }
     printf("Number of files = %d\n", count);
-    for (i = 1; i < count + 1; ++i) {
+    for (i = 1; i < count + 1; ++i)
+    {
         printf("\n%s", files[i - 1]->d_name);
     }
     printf("\n");
 }
 
-int file_select(struct direct *entry) {
+int file_select(struct direct *entry)
+{
     return (1);
 }
